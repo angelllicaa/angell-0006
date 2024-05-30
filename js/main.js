2ed9160432abd34de2ca3a7b8e5cf729e7e8c1ac
angelllicaa <165756144+angelllicaa@users.noreply.github.com>
2024-05-31 00:05:06 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const char = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+const playerScore = document.getElementById("score");
+
+
+let score = 0;
+let interval = null;
+
+let jumlahScore = ()=>{
+    score++;
+    playerScore.innerHTML = `Score : ${score}`
+};
+
+function jump(){
+    if(char.classList != "animate"){
+        char.classList.add("animate");
+    }
+    setTimeout(function(){
+        char.classList.remove("animate")
+    },500)
+    let score = 0
+    interval = setInterval(jumlahScore, 100)
+}
+
+const ifKitCactus = setInterval(function(){
+    const charTop = parseInt(window.getComputedStyle(char).getPropertyValue("top"))
+    const cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+    if(cactusLeft < 90 && cactusLeft > 0 && charTop >= 60){
+        cactus.style.animation = "none"
+        cactus.style.display = "none"
+        if(confirm("Sukur nabrak, lagi lagi🚗🚗")){
+            window.location.reload()
+        }
+    }
+})
